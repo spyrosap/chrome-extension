260aaf321461d408f4f1d10cfa20a3471298e547
spyrosap <spyrosap@hotmail.com>
2018-12-16 17:21:46 +0100

    bug fixc

--- background.js
+++ background.js
@@ -2,11 +2,11 @@ chrome.runtime.onInstalled.addListener(function() {
 
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function(){
         chrome.declarativeContent.onPageChanged.addRules([{
-            conditions:[new chrome.declarativeContent.pageStateMatcher({
+            conditions:[new chrome.declarativeContent.PageStateMatcher({
                 pageUrl:{hostEquals: 'https://www.youtube.com'}
             })
         ],
-            actions: [new chrome.declarativeContent.showPageAction()]
+            actions: [new chrome.declarativeContent.ShowPageAction()]
         }]);
     });
 });
